569afedade37732c97c8dad57b0432bf421779b4
weiserlab <weiserlab@weiserlabs-imac.ucdenver.pvt>
2020-06-25 17:18:05 -0600

    gwas

---
+++ gwas.py
@@ -0,0 +1,9 @@
+with open('cad.add.160614.website.txt') as gwas:
+    firstline = gwas.readline().rstrip()
+    lines = gwas.readlines()
+    with open('gwas_PTEN.txt', 'w') as gwas_pten:
+        gwas_pten.write(firstline)
+        for line in lines:
+            fields = line.split('\t')
+            if int(fields[1]) == 10 and int(fields[2]) >= 89623195 and int(fields[2]) <= 89728532:
+                gwas_pten.write(line)
